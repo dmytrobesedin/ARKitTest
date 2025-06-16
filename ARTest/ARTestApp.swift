e2e2d781e9e887e183dadd0475efc573904d036d
dmytrobesedin <dmytro.besedin@carecam.ai>
2025-06-16 22:48:02 +0300

    Initial Commit

---
+++ ARTest/ARTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTestApp.swift
+//  ARTest
+//
+//  Created by Dmytro Besedin on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ARTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
